0607f216ddc1b58e7e51e5cbcfc4a72916cbdffc
Valery <con-bo@yandex.ru>
2018-11-11 22:53:00 +0300

    remove the example tests

--- data/data/src/real/java/me/vponomarenko/ticketsapp/data/DataSource.kt
+++ data/data/src/real/java/me/vponomarenko/ticketsapp/data/DataSource.kt
@@ -1,4 +1,15 @@
 package me.vponomarenko.ticketsapp.data
 
-class DataSource {
+import me.vponomarenko.ticketsapp.data.entities.CityEntity
+import me.vponomarenko.ticketsapp.data.entities.FlightEntity
+import me.vponomarenko.ticketsapp.domain.search.data.City
+
+class DataSource : IDataSource {
+    override fun getCities(name: String): List<CityEntity> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getFlights(from: City, to: City): List<FlightEntity> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
